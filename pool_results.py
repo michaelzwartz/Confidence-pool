4a6378f4fb4b2a91dcdceb38809a5e45f6b6c926
Mike Zwartz <mtzwartz@gmail.com>
2022-09-17 14:08:05 -0500

    Update pool_results.py

--- pool_results.py
+++ pool_results.py
@@ -20,21 +20,42 @@ def combine(df1):
         i = i+2
     return my_file
 
-week1 = combine(week1_raw)
-week1.drop([3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43], axis=0, inplace=True)
+#function to count upsets, favoites, ties
+def upset_counter(df):
+    upset_count = 0
+    fav_count = 0
+    tie_count = 0
+    for col in week1.loc[:, ~week1.columns.isin(['Name', 'Score'])]:
+        if week1[col][3] < 0 and week1[col][1] == week1[col][0]: #if away team is favorite and home team won
+            upset_count = upset_count + 1
+        elif week1[col][3] > 0 and week1[col][2][0] == week1[col][0]: #if home team is favorite and away team won
+            upset_count = upset_count + 1
+        elif week1[col][0] == "TIE":
+            tie_count = tie_count + 1
+        else:
+            fav_count = fav_count + 1
+    
+    return upset_count, fav_count, tie_count
 
 #function to calculate total points
-i = 4
-week1['Score'] = 0
-while i < 42:
-    #print(i)
-    for col in week1.loc[:, ~week1.columns.isin(['Name', 'Score'])]:
-        if week1[col][0] == week1[col][i][0]:
-            week1['Score'][i] = week1['Score'][i] + week1[col][i][1]
-            #print(week1['Name'][i], ' the ', col, 'was a good game')
-        #else:
-            #print(week1['Name'][i], "haha, you suck")
-    i = i + 2
+def totals(df):
+    i = 4
+    df['Score'] = 0
+    while i < 42:
+        for col in df.loc[:, ~df.columns.isin(['Name', 'Score'])]:
+            if df[col][0] == df[col][i][0]:
+                df['Score'][i] = df['Score'][i] + df[col][i][1]
+        i = i + 2
+    return df
+
+
+week1 = combine(week1_raw)
+week1.drop([3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43], axis=0, inplace=True)
+week1 = totals(week1)
+
+upset_count_wk1 = upset_counter(week1)[0]
+fav_count_wk1 = upset_counter(week1)[1]
+tie_count_wk1 = upset_counter(week1)[2]
 
 #print scores in txt file (must be a string, not a df)
 #f=open(out_file, "w+")
