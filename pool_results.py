8f0e30669f1521d6933f32824cccec868f2c2ab9
Mike Zwartz <mtzwartz@gmail.com>
2022-10-12 21:17:20 -0500

    UPDATE

--- pool_results.py
+++ pool_results.py
@@ -1,63 +1,57 @@
 import pandas as pd
 import numpy as np
-import os.path
 
-out_file = "Week-1-Results.txt"
-
-print("Calculating the scores...")
-
-week1_raw = pd.read_csv("Pool_results.csv")
-
-#function to combine picks and bets
+#function to combine picks and point values
 def combine(df1):
     my_file = df1
     rows = my_file.shape[0]
     i = 2 
     while i < rows:
-        for col in my_file.loc[:, my_file.columns != 'Name']:
+        for col in my_file.loc[:, my_file.columns != 'Game']:
             my_file[col][i+1] = float(my_file[col][i+1])
             my_file[col][i] = [my_file[col][i], my_file[col][i+1]]
         i = i+2
+    
+    df1.drop([5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41], axis=0, inplace=True)
     return my_file
 
-#function to count upsets, favoites, ties
+#function to calculate totals
+def totals(df):
+    i = 4
+    df['Score'] = 0
+    df['Game Count'] = 0
+    df['Max Score'] = 136
+    while i < 41:
+        for col in df.loc[:, ~df.columns.isin(['Game', 'Score', 'Game Count', 'Max Score'])]:
+            if df[col][0] is np.nan:
+                break
+            
+            elif df[col][0] == df[col][i][0]:
+                df.loc[i, 'Game Count'] = df.loc[i, 'Game Count'] + 1
+                df.loc[i, 'Score'] = df.loc[i, 'Score'] + df.loc[i, col][1]
+            
+            else:  
+                df.loc[i, 'Max Score'] = df.loc[i, 'Max Score'] - df.loc[i, col][1]
+               
+        i = i + 2
+    return df
+
+#upset counter
 def upset_counter(df):
     upset_count = 0
     fav_count = 0
     tie_count = 0
-    for col in week1.loc[:, ~week1.columns.isin(['Name', 'Score'])]:
-        if week1[col][3] < 0 and week1[col][1] == week1[col][0]: #if away team is favorite and home team won
+    for col in df.loc[:, ~df.columns.isin(['Game', 'Score', 'Max Score'])]:
+        if df[col][3] < 0 and df[col][1] == df[col][0]: #if home team is favorite and away team won
             upset_count = upset_count + 1
-        elif week1[col][3] > 0 and week1[col][2][0] == week1[col][0]: #if home team is favorite and away team won
+            #print('Road upset alert ', df[col][0], "spread ", -(df[col][3]))
+        elif df[col][3] > 0 and df[col][2][0] == df[col][0]: #if away team is favorite and home team won
             upset_count = upset_count + 1
-        elif week1[col][0] == "TIE":
+            #print('Home dog win ', df[col][2])
+        elif df[col][0] == "TIE":
             tie_count = tie_count + 1
+            #print("A friggen tie...  ", df[col][2])
         else:
             fav_count = fav_count + 1
-    
+    print("Total of ", upset_count, "upsets this week")
     return upset_count, fav_count, tie_count
-
-#function to calculate total points
-def totals(df):
-    i = 4
-    df['Score'] = 0
-    while i < 42:
-        for col in df.loc[:, ~df.columns.isin(['Name', 'Score'])]:
-            if df[col][0] == df[col][i][0]:
-                df['Score'][i] = df['Score'][i] + df[col][i][1]
-        i = i + 2
-    return df
-
-
-week1 = combine(week1_raw)
-week1.drop([3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43], axis=0, inplace=True)
-week1 = totals(week1)
-
-upset_count_wk1 = upset_counter(week1)[0]
-fav_count_wk1 = upset_counter(week1)[1]
-tie_count_wk1 = upset_counter(week1)[2]
-
-#print scores in txt file (must be a string, not a df)
-#f=open(out_file, "w+")
-#f.write(week1[['Name', 'Score']])
-#f.close()
