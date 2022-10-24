c097e186762fdd07404a0d9c7d0e516c7725a748
Mike Zwartz <mtzwartz@gmail.com>
2022-10-23 21:48:15 -0500

    Update pool_results.py

--- pool_results.py
+++ pool_results.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 #function to combine picks and point values
 def combine(df1):
-    my_file = df1
+    my_file = df1.copy()
     rows = my_file.shape[0]
     i = 2 
     while i < rows:
@@ -12,15 +13,22 @@ def combine(df1):
             my_file[col][i] = [my_file[col][i], my_file[col][i+1]]
         i = i+2
     
-    df1.drop([5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41], axis=0, inplace=True)
+    my_file.drop([5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41], axis=0, inplace=True)
     return my_file
 
 #function to calculate totals
 def totals(df):
-    i = 4
+    max_score = 0
+    games = len(df.columns)-1
+    points = 16
+    while games > 0:
+        max_score = max_score + points
+        points = points - 1
+        games = games - 1
+    i = 4  
     df['Score'] = 0
     df['Game Count'] = 0
-    df['Max Score'] = 136
+    df['Max Score'] = max_score
     while i < 41:
         for col in df.loc[:, ~df.columns.isin(['Game', 'Score', 'Game Count', 'Max Score'])]:
             if df[col][0] is np.nan:
@@ -41,17 +49,53 @@ def upset_counter(df):
     upset_count = 0
     fav_count = 0
     tie_count = 0
+    dog_spread_tot = 0
+    big_dog_count = 0
     for col in df.loc[:, ~df.columns.isin(['Game', 'Score', 'Max Score'])]:
         if df[col][3] < 0 and df[col][1] == df[col][0]: #if home team is favorite and away team won
             upset_count = upset_count + 1
-            #print('Road upset alert ', df[col][0], "spread ", -(df[col][3]))
+            dog_spread_tot = dog_spread_tot + -(df[col][3])
+            print('Road upset alert ', df[col][0], "spread ", -(df[col][3]))
+            if abs(df[col][3]) > 6.5:
+                big_dog_count = big_dog_count + 1
+                print("big dog woof woof")
         elif df[col][3] > 0 and df[col][2][0] == df[col][0]: #if away team is favorite and home team won
             upset_count = upset_count + 1
-            #print('Home dog win ', df[col][2])
+            dog_spread_tot = dog_spread_tot + df[col][3]
+            print('Home dog win ', df[col][2])
+            if abs(df[col][3]) > 6.5:
+                big_dog_count = big_dog_count + 1
+                print("big dog woof woof")
         elif df[col][0] == "TIE":
             tie_count = tie_count + 1
-            #print("A friggen tie...  ", df[col][2])
+            print("A friggen tie...  ", df[col][2])
         else:
             fav_count = fav_count + 1
-    print("Total of ", upset_count, "upsets this week")
-    return upset_count, fav_count, tie_count
+    
+    dog_spread_avg = round(dog_spread_tot/upset_count, 2)
+    print("Total of ", upset_count, "upsets this week. With ", big_dog_count, "being big dogs." )
+    return upset_count, dog_spread_avg, big_dog_count #fav_count, tie_count
+
+#number of total picks for league
+def week_pick_percent(week_raw, week_game_total):
+    games = len(week_raw.axes[1])-(week_raw.iloc[0].isna().sum() + 1)
+    enteries = (len(week_raw.axes[0])-4)/2
+    total_picks = games * enteries
+    pick_percent = round(week_game_total/total_picks*100, 2)
+    
+    return pick_percent 
+
+#finds avg points per win for league
+def points_per_win (df):
+    total_wins = df['Game Count'].sum()
+    total_points = df['Score'].sum()
+    avg_pts_per_win = total_points/total_wins
+    points = round(avg_pts_per_win, 2)
+    return points
+
+#sum total points each week
+def sum_totals(season_totals, week_stats, week_number_rank):
+    season_totals[['Score', 'Game Count']] = season_totals[['Score', 'Game Count']] + week_stats[['Score', 'Game Count']]
+    season_totals[week_number_rank] = season_totals['Score'].rank(ascending=False, method='min')
+
+    return season_totals
